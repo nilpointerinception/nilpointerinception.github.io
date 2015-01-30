e67af84cdc6babfddcd54841ecfc83de46bc0c1a
Timur Uzel <nilpointerinception@gmail.com>
2015-01-29 19:24:32 -0500

    ajax'd it up

--- javascripts/ajax.js
+++ javascripts/ajax.js
@@ -1,7 +1,21 @@
 $(function() {
+
+  var newPost;
+  var count = 0;
+  var pages = ["1designer.html", "2rather.html", "3authors.html", "4support.html"];
+
   $("#ajaxButton").click(function() {
-    $.post("external.php", function(data) {
-      $("#name").html(data);
+    newPost = "<div id='post" + count + "' class='newPost'></div>";
+    $.post("html/" + pages[count], function(data) {
+      $("section").append(newPost);
+      $("#post" + count).html(data);
+      $("#post" + count).slideDown(1250);
+      $(location).attr('href', '#post' + count);
+      count++;
     });
+    if (count >= 3) {
+      $("#ajaxButton").fadeOut(800);
+    }
   });
+
 });
